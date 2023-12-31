845cea16cf5896c9ef7d6a452eaacb012176675a
VA_24 <24agnihotriv@smtexas.org>
2023-12-31 16:42:02 -0600

    remove scibowlscrim path

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Archives from './pages/Archives'
 import BLOGwinterbreakthoughts from './blog-posts/winter-break-thoughts'
 import BLOGideaboardvol1 from './blog-posts/ideaboard-vol-1'
 import BLOGboredom from './blog-posts/boredom'
-import ScibowlScrim from './hosted-projects/ScibowlScrim'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -21,7 +20,6 @@ function App() {
         <Route exact path='/archives/ideaboard-vol-1' element={<BLOGideaboardvol1 />}/>
         <Route exact path='/archives/boredom' element={<BLOGboredom />}/>
         <Route exact path='/hosted-projects' element={<HostedProjects />}/>
-        <Route exact path='/hosted-projects/scibowlscrim' element={<ScibowlScrim />}/>
       </Routes>
     </Router>
   );
