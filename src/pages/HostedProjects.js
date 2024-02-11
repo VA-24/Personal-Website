cba281bda190a273e291cb4db75120f92dcb4301
VA_24 <24agnihotriv@smtexas.org>
2024-02-11 12:43:04 -0600

    Update HostedProjects.js

--- src/pages/HostedProjects.js
+++ src/pages/HostedProjects.js
@@ -1,31 +1,31 @@
 import React, { useRef, useEffect } from "react";
 
 function HostedProjects() {
-  const textRef = useRef();
+  // const textRef = useRef();
 
-  useEffect(() => {
-    let dx = 2;
-    let dy = -2; // initial directions
-    let x = 0;
-    let y = 101;
-    let var_x = 10;
-    // change var_x to adjust for longer text
-    function animate() {
-      const textWidth = textRef.current.offsetWidth;
-      const textHeight = textRef.current.offsetHeight;
-      if (x + dx > window.innerWidth - textWidth - var_x || x + dx < 0) {
-        dx = -dx;
-      }
-      if (y + dy > window.innerHeight - textHeight - 1 || y + dy < 100) {
-        dy = -dy;
-      }
-      x += dx;
-      y += dy;
-      textRef.current.style.transform = `translate(${x}px, ${y}px)`;
-      requestAnimationFrame(animate);
-    }
-    animate();
-  }, []);
+  // useEffect(() => {
+  //   let dx = 2;
+  //   let dy = -2; // initial directions
+  //   let x = 0;
+  //   let y = 101;
+  //   let var_x = 10;
+  //   // change var_x to adjust for longer text
+  //   function animate() {
+  //     const textWidth = textRef.current.offsetWidth;
+  //     const textHeight = textRef.current.offsetHeight;
+  //     if (x + dx > window.innerWidth - textWidth - var_x || x + dx < 0) {
+  //       dx = -dx;
+  //     }
+  //     if (y + dy > window.innerHeight - textHeight - 1 || y + dy < 100) {
+  //       dy = -dy;
+  //     }
+  //     x += dx;
+  //     y += dy;
+  //     textRef.current.style.transform = `translate(${x}px, ${y}px)`;
+  //     requestAnimationFrame(animate);
+  //   }
+  //   animate();
+  // }, []);
 
   return (
 
@@ -63,17 +63,18 @@ function HostedProjects() {
                     </h1></a>
                 </main>
 
-                <div style={{ height: '1px', width: '100%', background: 'white', position: 'absolute', top: '98px' }}></div>
-
-            <a href='https://scibowlscrim.agno3.me'>
-                <h1
-                ref={textRef}
-                style={{ position: "absolute", top: 0, left: 0, color: "white" }}
-                className="translate"
-                >
-                  Projects coming soon, but for now, click me to look at the ScibowlScrim frontend skeleton!
+                <h1 class='text-xl xl:text-3xl font-medium text-neutral-100 mt-4 mb-11'>
+                  Plugs for ongoing projects:
                 </h1>
-              </a>
+
+                <a target='_blank' class='bg-white w-1/3 rounded-xl p-5 items-center' href='https://scibowlscrim.agno3.me/'>
+                    <div class='flex flex-col gap-4 items-center'>
+                        <h3 class='font-medium text-xl xl:text-2xl 2xl:text-3xl text-black'>ScibowlScrim</h3>
+                        <p class='text-base xl:text-lg 2xl:text-xl text-neutral-700 text-center'>Singleplayer out now! Access over 14,000 tossups from 8 different packet sets.</p>
+                    </div>
+                    
+                </a>
+
             </div>
 
         </body>
